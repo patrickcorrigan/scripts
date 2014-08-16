e656b402fe70956a344e8d4e26a9e39177135c32
Patrick Corrigan <patrickcorrigan7@gmail.com>
2014-08-16 12:40:57 +0100

    add new scripts

--- wifi-toggle.sh
+++ wifi-toggle.sh
@@ -1,13 +1,8 @@
+wifi_status_output=$(sudo ip link show wlp3s0)
 
-is_wifi_up ()
-{
-    output=$(sudo ip link show wlp3s0)
-    return  "$output" =~ "state Up" 
-}
-
-output = is_wifi_up
-if output then
+if [[ $wifi_status_output == *state\ UP* ]]
+then
     sudo ip link set wlp3s0 down
-else
+  else
     sudo ip link set wlp3s0 up
 fi
